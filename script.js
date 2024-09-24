45478852e274e8242541650ff7fe6c606db2b41b
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2024-09-24 14:37:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ function displaySentence(sentence, isSaved) {
 
     if (isSaved) {
                 const [english, spanish] = sentence.split('Español:'); // Split into English and Spanish
+                const [english] = english.slice('English:'); // Split into English and Spanish
+
+        
         sentenceContainer.innerHTML = `<strong>English:</strong> ${english}<br/><strong>Español:</strong> ${spanish}`;
         readSentence(english, 'en-US'); // Read the English part
         readSentence(spanish, 'es-ES'); // Read the Spanish part
